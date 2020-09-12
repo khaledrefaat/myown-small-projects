60115a239ffe97a693ea556191ea41be30a6341a
unknown <khaledrefaat08@gmail.com>
2020-09-13 01:46:54 +0200

    Delete Button Works

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -1,13 +1,29 @@
 const formInput_input = document.querySelector('input');
 const addButton_button = document.querySelector('.btn-add');
+const deleteButton_button = document.querySelector('.btn-delete');
 const todos_div = document.querySelector('.todos')
-const todo_div = document.querySelector('.todo')
+const todo_div = document.querySelectorAll('.todo')
 
-addButton_button.addEventListener('click', (e) => {
+
+// event Listeners
+addButton_button.addEventListener('click', addTodo)
+todos_div.addEventListener('click', deleteTodo);
+
+// functions
+
+function addTodo(e) {
     e.preventDefault();
     if (formInput_input.value !== '') {
-        todo_div.insertAdjacentHTML('beforeend', `<p class="todo">${formInput_input.value}<button class="btn btn-remove">X</button></p>`)
+        todos_div.insertAdjacentHTML('beforeend', `<p class="todo">${formInput_input.value}<button class="btn btn-delete">X</button></p>`)
         formInput_input.value = '';
     }
-})
+}
 
+function deleteTodo(e) {
+    console.log('i was clicked')
+    console.log(e.target);
+    if (e.target.innerHTML === 'X') {
+        let todo = e.target.parentNode;
+        todo.parentNode.removeChild(todo);
+    }
+}
