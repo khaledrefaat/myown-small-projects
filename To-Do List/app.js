659928076561b431942174d1105d1088c5b8ada0
unknown <khaledrefaat08@gmail.com>
2020-09-13 03:06:56 +0200

    complete button added and works

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -1,33 +1,29 @@
 const formInput_input = document.querySelector('input');
+const todoList_div = document.querySelector('.todo-list')
+const todo_div = document.querySelectorAll('.todo')
 const addButton_button = document.querySelector('.btn-add');
 const deleteButton_button = document.querySelector('.btn-delete');
 const clearButton_button = document.querySelector('.btn-clear');
-const todoList_div = document.querySelector('.todo-list')
-const todo_div = document.querySelectorAll('.todo')
+const completeButton_button = document.querySelector('.btn-complete');
 
 
 // event Listeners
 addButton_button.addEventListener('click', addTodo)
-todoList_div.addEventListener('click', deleteTodo);
-clearButton_button.addEventListener('click', clearBtn);
+todoList_div.addEventListener('click', todoBtns);
+clearButton_button.addEventListener('click', () => todoList_div.innerHTML = '');
 
 // functions
 
 function addTodo(e) {
     e.preventDefault();
     if (formInput_input.value !== '') {
-        todoList_div.insertAdjacentHTML('beforeend', `<div class="todo">${formInput_input.value}<button class="btn btn-delete">X</button></div>`)
+        todoList_div.insertAdjacentHTML('beforeend', `<div class="todo"><p>${formInput_input.value}</p><button class="btn btn-delete">X</button> <button class="btn btn-complete">✔</button></div>`)
         formInput_input.value = '';
     }
 }
 
-function deleteTodo(e) {
-    if (e.target.innerHTML === 'X') {
-        let todo = e.target.parentNode;
-        todo.parentNode.removeChild(todo);
-    }
+function todoBtns(e) {
+    if (e.target.innerHTML === 'X') e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    else if (e.target.innerHTML === '✔') e.target.parentNode.childNodes[0].classList.add('complete')
 }
 
-function clearBtn() {
-    todoList_div.innerHTML = '';
-}
