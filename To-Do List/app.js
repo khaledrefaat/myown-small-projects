107f670a103e4c8e2a2fc1cbb7fc465e35271a35
unknown <khaledrefaat08@gmail.com>
2020-09-13 02:05:53 +0200

    changing ui a little

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -1,27 +1,25 @@
 const formInput_input = document.querySelector('input');
 const addButton_button = document.querySelector('.btn-add');
 const deleteButton_button = document.querySelector('.btn-delete');
-const todos_div = document.querySelector('.todos')
+const todoList_div = document.querySelector('.todo-list')
 const todo_div = document.querySelectorAll('.todo')
 
 
 // event Listeners
 addButton_button.addEventListener('click', addTodo)
-todos_div.addEventListener('click', deleteTodo);
+todoList_div.addEventListener('click', deleteTodo);
 
 // functions
 
 function addTodo(e) {
     e.preventDefault();
     if (formInput_input.value !== '') {
-        todos_div.insertAdjacentHTML('beforeend', `<p class="todo">${formInput_input.value}<button class="btn btn-delete">X</button></p>`)
+        todoList_div.insertAdjacentHTML('beforeend', `<div class="todo">${formInput_input.value}<button class="btn btn-delete">X</button></div>`)
         formInput_input.value = '';
     }
 }
 
 function deleteTodo(e) {
-    console.log('i was clicked')
-    console.log(e.target);
     if (e.target.innerHTML === 'X') {
         let todo = e.target.parentNode;
         todo.parentNode.removeChild(todo);
