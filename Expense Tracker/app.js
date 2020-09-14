7859d24d8643ad4612ad052dee0ae5fbd4158d7b
unknown <khaledrefaat08@gmail.com>
2020-09-14 06:03:02 +0200

    delete button works

--- Expense Tracker/app.js
+++ Expense Tracker/app.js
@@ -1,26 +1,37 @@
 const inputName_input = document.querySelector('.input-name');
 const inputDate_input = document.querySelector('.input-date');
-const inputAmound_input = document.querySelector('.input-amount');
+const inputAmount_input = document.querySelector('.input-amount');
 const btnAdd_btn = document.querySelector('.btn-add');
-const btnDelete_btn = document.querySelector('.btn-delete');
 const expenseContainer = document.querySelector('.expense-container');
 
 
 // event Listeners
 btnAdd_btn.addEventListener('click', addExpense);
+expenseContainer.addEventListener('click', removeExpense);
 
 // functions
 
 function addExpense(e) {
     e.preventDefault();
-    if (inputName_input.value !== '' && inputDate_input.value !== '' && inputAmound_input.value !== '') {
+    if (inputName_input.value !== '' && inputDate_input.value !== '' && inputAmount_input.value !== '') {
         expenseContainer.insertAdjacentHTML('beforeend', `
                 <div class="expense-details">
-                    <div>${inputName_input.value}'s</div>
+                    <div>${inputName_input.value}</div>
                     <div>${inputDate_input.value}</div>
-                    <div>${inputAmound_input.value}$</div>
+                    <div>${inputAmount_input.value}$</div>
                     <button class="btn btn-delete">X</button>
                 </div>
         `)
+        inputName_input.value = '';
+        inputDate_input.value = '';
+        inputAmount_input.value = '';
+    }
+}
+
+function removeExpense(e) {
+    if (e.target.value = 'X') {
+        e.preventDefault();
+        let expense = e.target.parentNode
+        expense.remove()
     }
 }
