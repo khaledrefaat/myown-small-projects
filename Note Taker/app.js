470f30eeac4f836e925876a1669a901beb6f1e0b
unknown <khaledrefaat08@gmail.com>
2020-09-19 09:56:16 +0200

    popup ui finish

--- Note Taker/app.js
+++ Note Taker/app.js
@@ -4,18 +4,28 @@ const btnDetails_div = document.querySelector('.btn-details');
 const noteContainer_div = document.querySelector('.note-container');
 let i = 1;
 
-btnAdd_btn.addEventListener('click', e => {
+
+// event Listeners
+
+btnAdd_btn.addEventListener('click', addNote);
+
+
+// functions
+
+
+function addNote(e) {
     e.preventDefault();
     if (noteInput_textarea.value !== '') {
         if (!noteContainer_div.innerHTML.includes('<div class="note">')) noteContainer_div.innerHTML = '';
+        // if the note length more than 15 chars then replace the rest with ...
         let note = noteInput_textarea.value.length > 15 ? noteInput_textarea.value.slice(0, 15) + '...' : noteInput_textarea.value;
         noteContainer_div.insertAdjacentHTML('beforeend', `
         <div class="note">
             <h3>note ${i}</h3>
             <p>${note}</p>
-            <div class="btn btn-details">view details</div>
+            <button class="btn btn-details">view details</button>
         </div>
         `)
         i++;
     }
-})
+}
